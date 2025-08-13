0c1e9f76198435b89566e6bab3c46487ab30850e
Ammar Ahmed <ammarahmed132004@outlook.com>
2025-08-13 14:52:04 +0300

    ED

--- pages/history.php
+++ pages/history.php
@@ -116,6 +116,9 @@ $total_users = $mysqli->query("SELECT COUNT(*) AS total_users FROM Users WHERE d
 
     <div x-show="isTaskModalModal" x-transition=""
         class="fixed inset-0 flex items-center justify-center p-5 overflow-y-auto z-99999">
+
+
+
         <div class="fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px]" @click="isTaskModalModal = false">
         </div>
 
@@ -163,25 +166,32 @@ $total_users = $mysqli->query("SELECT COUNT(*) AS total_users FROM Users WHERE d
                 <?php require('../includes/nav.php'); ?>
                 <div class="mx-auto max-w-(--breakpoint-2xl) p-4 md:p-6">
 
-                    <div class="px-6 py-4">
-                        <p class="flex items-center gap-3 text-gray-500 dark:text-gray-400 justify-between">
-                            السجل الكامل
 
+                    <div class="mb-8 flex flex-col justify-between gap-4  flex-row items-center">
 
+                        <h3 class="text-lg font-semibold text-gray-800 dark:text-white"> السجل الكامل</h3>
 
-
-                            <span
-                                class="bg-success-50 text-success-600 dark:bg-success-500/15 dark:text-success-500 inline-flex items-center justify-center gap-1 rounded-full px-2.5 py-0.5 text-sm font-medium">
-                                <?= $selected_date ?> </span>
-
-
-
-                        </p>
-
-
-
-
+                        <div>
+                            <button @click="isTaskModalModal = true"
+                                class="text-theme-sm shadow-theme-xs inline-flex h-10 items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
+                                <svg class="stroke-current fill-white dark:fill-gray-800" width="20" height="20"
+                                    viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2.29004 5.90393H17.7067" stroke="" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M17.7075 14.0961H2.29085" stroke="" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path
+                                        d="M12.0826 3.33331C13.5024 3.33331 14.6534 4.48431 14.6534 5.90414C14.6534 7.32398 13.5024 8.47498 12.0826 8.47498C10.6627 8.47498 9.51172 7.32398 9.51172 5.90415C9.51172 4.48432 10.6627 3.33331 12.0826 3.33331Z"
+                                        fill="" stroke="" stroke-width="1.5"></path>
+                                    <path
+                                        d="M7.91745 11.525C6.49762 11.525 5.34662 12.676 5.34662 14.0959C5.34661 15.5157 6.49762 16.6667 7.91745 16.6667C9.33728 16.6667 10.4883 15.5157 10.4883 14.0959C10.4883 12.676 9.33728 11.525 7.91745 11.525Z"
+                                        fill="" stroke="" stroke-width="1.5"></path>
+                                </svg>
+                                سجل النشاط
+                            </button>
+                        </div>
                     </div>
+
 
 
                     <div
@@ -318,25 +328,11 @@ $total_users = $mysqli->query("SELECT COUNT(*) AS total_users FROM Users WHERE d
                             <p class="flex items-center gap-3 text-gray-500 dark:text-gray-400 justify-between">
                                 السجل اليومي
 
-                                <button @click="isTaskModalModal = true"
-                                    class="text-theme-sm shadow-theme-xs inline-flex h-10 items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
-                                    <svg class="stroke-current fill-white dark:fill-gray-800" width="20" height="20"
-                                        viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2.29004 5.90393H17.7067" stroke="" stroke-width="1.5"
-                                            stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path d="M17.7075 14.0961H2.29085" stroke="" stroke-width="1.5"
-                                            stroke-linecap="round" stroke-linejoin="round"></path>
-                                        <path
-                                            d="M12.0826 3.33331C13.5024 3.33331 14.6534 4.48431 14.6534 5.90414C14.6534 7.32398 13.5024 8.47498 12.0826 8.47498C10.6627 8.47498 9.51172 7.32398 9.51172 5.90415C9.51172 4.48432 10.6627 3.33331 12.0826 3.33331Z"
-                                            fill="" stroke="" stroke-width="1.5"></path>
-                                        <path
-                                            d="M7.91745 11.525C6.49762 11.525 5.34662 12.676 5.34662 14.0959C5.34661 15.5157 6.49762 16.6667 7.91745 16.6667C9.33728 16.6667 10.4883 15.5157 10.4883 14.0959C10.4883 12.676 9.33728 11.525 7.91745 11.525Z"
-                                            fill="" stroke="" stroke-width="1.5"></path>
-                                    </svg>
-                                    سجل النشاط
-                                </button>
 
 
+                                <span
+                                    class="bg-success-50 text-success-600 dark:bg-success-500/15 dark:text-success-500 inline-flex items-center justify-center gap-1 rounded-full px-2.5 py-0.5 text-sm font-medium">
+                                    <?= $selected_date ?> </span>
 
 
 
@@ -366,10 +362,10 @@ $total_users = $mysqli->query("SELECT COUNT(*) AS total_users FROM Users WHERE d
                                             السعر</th>
                                         <th
                                             class="px-6 py-4 text-left text-sm font-medium whitespace-nowrap text-gray-500 dark:text-gray-400">
-                                            المبلغ المدفوع</th>
+                                            المدفوع</th>
                                         <th
                                             class="px-6 py-4 text-left text-sm font-medium whitespace-nowrap text-gray-500 dark:text-gray-400">
-                                            المبلغ المتبقي</th>
+                                            المتبقي</th>
                                         <th
                                             class="px-6 py-4 text-left text-sm font-medium whitespace-nowrap text-gray-500 dark:text-gray-400">
                                             وقت الحجز</th>
